c2bb1c11d6a294f8367893b49c24a1ce22dbad8a
Don Welch <dwelch@nait.ca>
2020-02-27 13:50:16 -0700

    Introduction to web pages. Code separate menu in web.sitemap, Implement in master

---
+++ BasicWebApp/WebApp/SamplePages/HelloWorld.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="Hello World" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true"
+    CodeBehind="HelloWorld.aspx.cs" Inherits="WebApp.SamplePages.HelloWorld" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Hello World</h1>
+    <asp:Label ID="PromptLabel" runat="server" Text="Enter your name"></asp:Label>
+    &nbsp;&nbsp;
+    <asp:TextBox ID="NameArg" runat="server" ToolTip="Enter your name"
+         placeholder="your name"></asp:TextBox>
+    &nbsp;&nbsp;
+    <asp:Button ID="PressMe" runat="server" Text="Press Me"
+         CssClass="btn btn-primary" OnClick="PressMe_Click"/>
+    <br />
+    <asp:Label ID="OutputArea" runat="server"></asp:Label>
+    <br />
+    </asp:Content>
---
+++ BasicWebApp/WebApp/SamplePages/HelloWorld.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp.SamplePages
+{
+    public partial class HelloWorld : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void PressMe_Click(object sender, EventArgs e)
+        {
+            //when the PressMe button is pressed, this code
+            //   will be executed
+            //this is called event-driven logic
+            //the event was the pressing of the button
+            //sees the Onclick property of your control on the
+            //    web page.
+
+            //The ID name of a control acts the the variable name
+            //   in your code
+            //Since, each control is a class instance, access to the
+            //   contents of a class is by the class properties
+            //Just like Razor, 95% of all data content is returned 
+            //   as a string
+            if (string.IsNullOrEmpty(NameArg.Text))
+            {
+                OutputArea.Text = "Enter your name, please";
+            }
+            else
+            {
+                OutputArea.Text = "hello " + NameArg.Text;
+                OutputArea.ForeColor = System.Drawing.Color.Red;
+                OutputArea.Font.Size = 100; 
+            }
+        }
+    }
+}
